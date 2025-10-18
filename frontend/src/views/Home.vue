e3a5b456e1c2599155a60c23749ab30e79061775
Niklas Merz <niklas@merz.dev>
2025-10-18 11:20:13 +0200

    no shutdown

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <shutdown-de v-if="$i18n.locale === 'de' || $i18n.locale === 'de-at'"></shutdown-de>
-    <shutdown-en v-else></shutdown-en>
     <home-de v-if="$i18n.locale === 'de' || $i18n.locale === 'de-at'"></home-de>
     <home-en v-else></home-en>
     <calendar></calendar>
@@ -17,8 +15,6 @@ import FaqDe from '../i18n/views/FAQ.de.vue'
 import FaqEn from '../i18n/views/FAQ.en.vue'
 import HomeDe from '../i18n/views/Home.de.vue'
 import HomeEn from '../i18n/views/Home.en.vue'
-import ShutdownDe from '../i18n/views/Shutdown.de.vue'
-import ShutdownEn from '../i18n/views/Shutdown.en.vue'
 import utils from '../utils'
 
 export default {
@@ -28,8 +24,6 @@ export default {
     FaqEn,
     HomeDe,
     HomeEn,
-    ShutdownDe,
-    ShutdownEn
   },
   created: function() {
     utils.clearTitle()
